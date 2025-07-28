9e238b0f2fb7e760a88f989dac176149e30c2b0e
etorreborre <etorreborre@yahoo.com>
2025-07-28 08:37:20 +0200

    feature: add a flox cmt.nix file

---
+++ .flox/pkgs/cmt.nix
@@ -0,0 +1,21 @@
+{ pkgs, stdenv, lib, haskellPackages }:
+with haskellPackages;
+  mkDerivation rec {
+    pname = "cmt";
+    version = "0.7.1.1";
+    src = ../../.;
+    isExecutable = true;
+    libraryHaskellDepends = [
+      ansi-terminal attoparsec base classy-prelude containers directory
+      file-embed filepath process terminal-size text
+    ];
+    executableHaskellDepends = [ base classy-prelude ];
+
+    doHaddock = false;
+    doCheck = false;
+
+    homepage = "https://github.com/smallhadroncollider/cmt#readme";
+    description = "Write consistent git commit messages";
+    license = lib.licenses.bsd3.spdxId;
+    mainProgram = "cmt";
+  }
